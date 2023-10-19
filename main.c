f70e99e31dbadb95b7c9a91f4974ea421355529e
seocoal <jiyoon369@sookmyung.ac.kr>
2023-10-19 15:38:27 +0900

    3

--- main.c
+++ main.c
@@ -3,17 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
+int inc(int counter);
 int main(int argc, char *argv[]) {
 	
-	int i;
-	
-	for(i=0;i<5;i++)
-	{
-		int temp=1;
-		printf("temp=%d\n", temp);
-		temp++;
-		
-	}
-	
+	int i=10;
+	printf("i before function call = %d\n", i);
+	inc(i);
+	printf("i after function call = %d\n", i);
+
 	return 0;
+}
+
+int inc(int counter){
+	
+	counter++;
+	return counter;
+	
 }
