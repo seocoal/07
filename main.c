27706ed92ebd1274405ad82ca68978226f2ae745
seocoal <jiyoon369@sookmyung.ac.kr>
2023-10-19 17:43:16 +0900

    6-1

--- main.c
+++ main.c
@@ -5,25 +5,17 @@
 
 
 
-void sub(void);
+int all_files;
+
+extern void sub();
 
 int main(int argc, char *argv[]) 
 {
-	int i;
-	for (i=0;i<3;i++)
+	sub();
+	printf("%d \n", all_files);
 	
-	  sub();
 	  
 	return 0;
 }
 
-void sub(void)
-{
-	int auto_count=0;
-	static int static_count=0;
-	auto_count++;
-	static_count++;
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n", static_count);
-	
-}
+
