86e34aa597c6011f2879160d47ee1d6c90094c37
seocoal <jiyoon369@sookmyung.ac.kr>
2023-10-19 15:24:42 +0900

    2

--- main.c
+++ main.c
@@ -5,17 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int flag =1;
-	while(flag != 0)
+	int i;
+	
+	for(i=0;i<5;i++)
 	{
-		int y;
-		y=3;
-		flag=0;
+		int temp=1;
+		printf("temp=%d\n", temp);
+		temp++;
 		
 	}
-	
-	int y;
-	y=4;
 	
 	return 0;
 }
