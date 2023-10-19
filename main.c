4bf93f3e42c7faa0012fff0799950cd759c98037
seocoal <jiyoon369@sookmyung.ac.kr>
2023-10-19 16:22:05 +0900

    5-2

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 void sub(void)
 {
 	int auto_count=0;
-	int static_count=0;
+	static int static_count=0;
 	auto_count++;
 	static_count++;
 	printf("auto_count=%d\n", auto_count);
